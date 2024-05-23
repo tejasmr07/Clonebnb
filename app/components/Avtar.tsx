3e86f28149ed486b28c84c8c7b689251ca0aee25
tejasmr07 <tejasmr49@gmail.com>
2024-05-23 21:01:35 +0530

    inserting into Navbar

---
+++ app/components/Avtar.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Image from "next/image";
+
+const Avtar = () => {
+  return (
+    <Image
+      className="rounded-full"
+      height="30"
+      width="30"
+      alt="Avatar"
+      src="/images/placeholder.jpg"
+    />
+  );
+};
+
+export default Avtar;
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,8 @@
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
+
 const Navbar = () => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
@@ -16,6 +19,8 @@ const Navbar = () => {
           "
           >
             <Logo />
+            <Search />
+            <UserMenu />
           </div>
         </Container>
       </div>
